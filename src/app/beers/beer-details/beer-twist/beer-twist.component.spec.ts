097636852b854b002ceb7c73f0d8fbe865f549dd
Nestor Moschen <nestor.moschen@gmail.com>
2022-05-16 18:46:25 +0100

    test: add test cases for twist component

--- src/app/beers/beer-details/beer-twist/beer-twist.component.spec.ts
+++ src/app/beers/beer-details/beer-twist/beer-twist.component.spec.ts
@@ -1,10 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { BeerTwistComponent } from './beer-twist.component';
 
 describe('BeerTwistComponent', () => {
   let component: BeerTwistComponent;
   let fixture: ComponentFixture<BeerTwistComponent>;
+
+  const page = {
+    get title(): HTMLTitleElement {
+      return fixture.debugElement.query(By.css('#twist-title')).nativeElement;
+    },
+    get info(): HTMLParagraphElement {
+      return fixture.debugElement.query(By.css('#twist-info')).nativeElement;
+    },
+  }
+  const twist = 'Some random text over here! Thanks for reading :p';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +26,14 @@ describe('BeerTwistComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BeerTwistComponent);
     component = fixture.componentInstance;
+
+    component.twist = twist;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show the twist details', () => {
+    expect(page.title.innerText).toBe('Twist');
+    expect(page.info.innerText).toBe(twist);
   });
 });
