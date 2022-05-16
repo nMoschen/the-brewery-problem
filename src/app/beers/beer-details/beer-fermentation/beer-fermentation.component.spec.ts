20222b7fdf4793161908fc82a446903b0756e04c
Nestor Moschen <nestor.moschen@gmail.com>
2022-05-16 18:44:34 +0100

    test: add test cases for beer fermentation

--- src/app/beers/beer-details/beer-fermentation/beer-fermentation.component.spec.ts
+++ src/app/beers/beer-details/beer-fermentation/beer-fermentation.component.spec.ts
@@ -1,10 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { BeerFermentationMocker } from '../../testing';
 import { BeerFermentationComponent } from './beer-fermentation.component';
 
 describe('BeerFermentationComponent', () => {
   let component: BeerFermentationComponent;
   let fixture: ComponentFixture<BeerFermentationComponent>;
+
+  const page = {
+    get title(): HTMLTitleElement {
+      return fixture.debugElement.query(By.css('#fermentation-title')).nativeElement;
+    },
+    get fermentation(): HTMLDivElement {
+      return fixture.debugElement.query(By.css('#fermentation-temperature')).nativeElement;
+    },
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +26,14 @@ describe('BeerFermentationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BeerFermentationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show the fermentation details', () => {
+    const fermentation = BeerFermentationMocker.generateOne();
+    component.fermentation = fermentation;
+    fixture.detectChanges();
+
+    expect(page.title.innerText).toBe('Fermentation');
+    expect(page.fermentation.innerText).toBe(`${fermentation.temp.value}° ${fermentation.temp.unit}`);
   });
 });
