69a1ce2548edc38c769561481af03f469c618229
Nestor Moschen <nestor.moschen@gmail.com>
2022-05-16 18:48:25 +0100

    test: add test cases for beer mashing component

--- src/app/beers/beer-details/beer-mashing/beer-mashing.component.spec.ts
+++ src/app/beers/beer-details/beer-mashing/beer-mashing.component.spec.ts
@@ -1,10 +1,21 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { BeerMashingMocker } from '../../testing';
 import { BeerMashingComponent } from './beer-mashing.component';
 
 describe('BeerMashingComponent', () => {
   let component: BeerMashingComponent;
   let fixture: ComponentFixture<BeerMashingComponent>;
+
+  const page = {
+    get title(): HTMLTitleElement {
+      return fixture.debugElement.query(By.css('#mashing-title')).nativeElement;
+    },
+    get infos(): DebugElement[] {
+      return fixture.debugElement.queryAll(By.css('.mashing-info'));
+    },
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +27,16 @@ describe('BeerMashingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BeerMashingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show the mashing details', () => {
+    const mashing = BeerMashingMocker.generateList(4);
+    component.mashing = mashing;
+    fixture.detectChanges();
+
+    expect(page.title.innerText).toBe('Mashing');
+    page.infos.forEach((element, index) => {
+      expect(element.nativeElement.innerText).toBe(`${mashing[index].temp.value}° ${mashing[index].temp.unit} (${mashing[index].duration} min)`);
+    });
   });
 });
