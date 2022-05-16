4fa975f094ef9a045e3804c255dac957acd56b1f
Nestor Moschen <nestor.moschen@gmail.com>
2022-05-16 19:56:32 +0100

    test: add missing dependency to beer component test file

--- src/app/beers/beers.component.spec.ts
+++ src/app/beers/beers.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { BeersComponent } from './beers.component';
 
 describe('BeersComponent', () => {
@@ -8,7 +8,8 @@ describe('BeersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BeersComponent ]
+      declarations: [ BeersComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   });
